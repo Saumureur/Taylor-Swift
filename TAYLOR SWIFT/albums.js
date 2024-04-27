cde334ade13dbc49e6f818bfc05f502a3d2485cf
Saumureur <93099074+Saumureur@users.noreply.github.com>
2024-04-27 19:19:22 +0200

    add js

---
+++ TAYLOR SWIFT/albums.js
@@ -0,0 +1,118 @@
+albums = [
+    {
+        "name": "1989",
+        "imagesPath": [
+            "./images/1989.jpg",
+            "./images/1989-2.jpg"
+        ],
+        "songs": [
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Shake it Off",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "18 août 2014"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Blank Space",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "10 novembre 2014"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Style",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "9 février 2015"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Bad Blood",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "17 mai 2015"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Wildest Dreams",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "30 août 2015"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Out Of the Woods",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "31 décembre 2015"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "New Romantics",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "13 avril 2016"
+            }
+        ]
+    },
+    {
+        "name": "evermore",
+        "imagesPath": [
+            "./images/evermore.webp"
+        ],
+        "songs": [
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "willow",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "11 décembre 2020"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "no body, no crime (feat. Haim)",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "11 janvier 2021"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "coney island (feat. The National)",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "18 janvier 2021"
+            }
+        ]
+    },
+    {
+        "name": "Fearless",
+        "imagesPath": [
+            "./images/fearless1.jpeg",
+            "./images/fearless2.jpg"
+        ],
+        "songs": [
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Love Story",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "15 septembre 2008"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "White Story",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "9 décembre 2008"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "You Belong With Me",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "4 novembre 2008"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Fifteen",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "30 août 2009"
+            },
+            {
+                "id": "0cqRj7pUJDkTCEsJkx8snD",
+                "name": "Fearless",
+                "lyricsPath": "./lyrics/0cqRj7pUJDkTCEsJkx8snD.txt",
+                "dateSortie": "3 janvier 2010"
+            }
+        ]
+    }
+]
